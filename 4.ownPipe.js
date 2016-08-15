fce86a7706b539cd6a701c927ddc1f9bd0c57073
Dzyuba Vlad <Dzyubavlad@gmail.com>
2016-08-15 16:13:32 +0300

    Added example of writing own pipe

---
+++ 4.ownPipe.js
@@ -0,0 +1,29 @@
+var fs = require('fs'),
+    path = require('path');
+
+function concatFilesIn(sources, destination) {
+  let destStream = fs.createWriteStream(destination);
+  var endedStreamsCount = 0;
+  sources.map(x => fs.createReadStream(x))
+         .forEach(sourceStream => {
+            sourceStream.on('end', _ => {
+              endedStreamsCount++;
+              if (endedStreamsCount === sources.length) destStream.end();
+            });
+            pipe(sourceStream, destStream);
+         });
+}
+
+function pipe(sourceStream, destinationStream) {
+  sourceStream.on('data', data => destinationStream.write(data));
+}
+
+function inLogs(filename) {
+  return 'logs'+path.sep+filename;
+}
+
+concatFilesIn([
+  '2016-08-05-error.log',
+  '2016-08-05-node.log',
+  '2016-08-05-server.log'
+].map(inLogs), inLogs('2016-08-05.log'));
